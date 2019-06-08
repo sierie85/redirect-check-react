6d1f05de30aefb941ada3429b526a40ba4112877
s.brieschenk <s.brieschenk@neodesign.tv>
2019-06-08 10:26:22 +0200

    change url

--- src/helpers.js
+++ src/helpers.js
@@ -9,8 +9,7 @@ const awaitPapa = async file => {
 
 const redirectCheck = async (domain, resource, redirect) => {
   const rawResponse = await fetch(
-    "http://localhost:5000/check-redirect",
-    // "https://redirect-tool.herokuapp.com/check-redirect",
+    "https://redirect-tool.herokuapp.com/check-redirect",
     {
       method: "POST",
       headers: {
